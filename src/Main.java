a62115a6860721dedf1fb0898406e7c5de928d3f
Amzad <Amzad@amzadchowdhury.com>
2018-08-10 23:55:23 -0400

    Almost done again. Need to compute the comparisons.

--- src/Main.java
+++ src/Main.java
@@ -1,103 +1,187 @@
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Scanner;
 import java.util.Timer;
 
 public class Main {
 
-	public static long time = System.currentTimeMillis();
-	public static Timer timer = new Timer();
+	public static Instant start;
+	public static Instant stop;
+	public static int complexity = 0;
+	public static String[] data = { "", "" };
+	public static long time;
 
+
+	public static void main(String[] args) {
+		print("Naive vs Advanced Pattern Matching");
+		print("By Amzad Chowdhury");
+		print("Pick an option below:");
+		while (true) {
+			print("1 - Naive Pattern Matching Algorithm");
+			print("2 - KMP Pattern Algorithm");
+			print("3 - Rabin-Karp Hashing Algorithm");
+			print("4 - All 3 Concurrently with same random input");
+			print("5 - Change random data complexity. Current at: " + complexity);
+
+			String input = null;
+			Scanner in = new Scanner(System.in);
+			while (true) {
+				try {
+
+					input = in.nextLine();
+					// print(input);
+					if (Integer.parseInt(input) == 1) {
+						generateData();
+						startTime();
+						search(data[0], data[1]);
+						printTime();
+						break;
+
+					} else if (Integer.parseInt(input) == 2) {
+						generateData();
+						startTime();
+						KMPSearch(data[1], data[0]);
+						printTime();
+						break;
+
+					} else if (Integer.parseInt(input) == 3) {
+						generateData();
+						startTime();
+						searchRabinKarp(data[1], data[0], 101);
+						printTime();
+						
+
+					} else if (Integer.parseInt(input) == 4) {
+						generateData();
+						long[] results = new long[3]; 
+						
+						print("Naive Pattern Algorithm");
+						startTime();
+						search(data[0], data[1]);
+						results[0] = (System.nanoTime() - time);
+						print("\n");
+						
+						print("KMP Algorithm");
+						startTime();
+						search(data[0], data[1]);
+						results[1] = (System.nanoTime() - time);
+						print("\n");
+						
+						print("Rabin-Karp Algorithm");
+						startTime();
+						searchRabinKarp(data[1], data[0], 101);
+						results[2] = (System.nanoTime() - time);
+						print("\n");
+						
+						print("Naive Pattern Algorithm Time: " + results[0]);
+						print("KMP Search Algorithm Time: " + results[1]);
+						print("Rabin-Karp Algorithm Time: " + results[2]);
+
+					} else if (Integer.parseInt(input) == 5) {
+						print("The complexity level determines the length of the string and pattern.");
+						print("0 - String length 100, Pattern length 2");
+						print("1 - String length 200, Pattern length 4");
+						print("2 - String length 400, Pattern length 6");
+						print("Enter the desired complexity level");
+						input = in.nextLine();
+						
+						if (Integer.parseInt(input) == 0) {
+							complexity = 0;
+							break;
+						} else if (Integer.parseInt(input) == 1) {
+							complexity = 1;
+							break;
+						} else if (Integer.parseInt(input) == 2) {
+							complexity = 2;
+							break;
+						} else {
+							print("Invalid Input. Try again");
+						}
+						
+					} else {
+						print("Invalid Input. Try again");
+					}
+				} catch (NumberFormatException e) {
+					print("Invalid Input. Try again");
+					continue;
+				}
+			}
+
+		}
+
+	}
+	
 	public static void print(String x) {
 		System.out.println(x);
 	}
 
-	public static void printT(String x) {
-		System.out.println("[" + (System.currentTimeMillis() - time) + "] : " + x);
-	}
-	
-	public static void printS(String x) {
-		System.out.println(timer);
-	}
-
-	public static String[] generateData(int complexity) {
-		String[] data = { "", ""};
+	public static void generateData() {
 		String full = "ABCD";
-		
+
 		if (complexity == 0) {
 			String temp = "";
-			
-			// Create a String with length 20
-			for (int i = 0; i < 20; i++) {
-				temp = temp + full.charAt((int) (Math.random() * ( full.length() - 0 )));
+
+			// Create a String with length 100
+			for (int i = 0; i < 100; i++) {
+				temp = temp + full.charAt((int) (Math.random() * (full.length() - 0)));
 			}
-			
+
 			// Create a pattern with length 2
 			String tempPattern = "";
 			for (int i = 0; i < 2; i++) {
-				tempPattern = tempPattern + full.charAt((int) (Math.random() * ( full.length() - 0 )));
+				tempPattern = tempPattern + full.charAt((int) (Math.random() * (full.length() - 0)));
 			}
 
 			data[0] = temp;
 			data[1] = tempPattern;
-			printT("String Created: " + data[0]);
-			printT("Pattern Created: " + data[1]);
-		}
-		else if (complexity == 1) {
+			print("String Created: " + data[0]);
+			print("Pattern Created: " + data[1]);
+		} else if (complexity == 1) {
 			String temp = "";
-			
-			// Create a String with length 30
-			for (int i = 0; i < 30; i++) {
-				temp = temp + full.charAt((int) (Math.random() * ( full.length() - 0 )));
+
+			// Create a String with length 200
+			for (int i = 0; i < 200; i++) {
+				temp = temp + full.charAt((int) (Math.random() * (full.length() - 0)));
 			}
-			
+
 			// Create a pattern with length 4
 			String tempPattern = "";
 			for (int i = 0; i < 4; i++) {
-				tempPattern = tempPattern + full.charAt((int) (Math.random() * ( full.length() - 0 )));
+				tempPattern = tempPattern + full.charAt((int) (Math.random() * (full.length() - 0)));
 			}
 
 			data[0] = temp;
 			data[1] = tempPattern;
-			printT("String Created: " + data[0]);
-			printT("Pattern Created: " + data[1]);
-		}
+			print("String Created: " + data[0]);
+			print("Pattern Created: " + data[1]);
+		} else if (complexity == 2) {
+			String temp = "";
 
-		return data;
-	}
-
-	public static void main(String[] args) {
-
-		print("Naive vs Advanced Pattern Matching");
-		print("By Amzad Chowdhury");
-		print("Pick an option below:");
-		print("1 - Naive Pattern Matching Algorithm");
-		print("2 - KMP Pattern Algorithm");
-		print("3 - Rabin-Karp Hashing Algorithm");
-		print("4 - All 3 Concurrently with same random input");
-
-		String input = null;
-		Scanner in = new Scanner(System.in);
-		while (true) {
-			try {
-				
-				input = in.nextLine();
-				//print(input);
-				if (Integer.parseInt(input) == 1) {
-					String[] dapta = generateData(0);
-
-				} else if (Integer.parseInt(input) == 2) {
-
-				} else if (Integer.parseInt(input) == 3) {
-
-				} else if (Integer.parseInt(input) == 4) {
-
-				} else {
-					print("Invalid Input. Try again");
-				}
-			} catch (NumberFormatException e) {
-				print("Invalid Input. Try again");
-				continue;
+			// Create a String with length 400
+			for (int i = 0; i < 400; i++) {
+				temp = temp + full.charAt((int) (Math.random() * (full.length() - 0)));
 			}
+
+			// Create a pattern with length 6
+			String tempPattern = "";
+			for (int i = 0; i < 6; i++) {
+				tempPattern = tempPattern + full.charAt((int) (Math.random() * (full.length() - 0)));
+			}
+
+			data[0] = temp;
+			data[1] = tempPattern;
+			print("String Created: " + data[0]);
+			print("Pattern Created: " + data[1]);
 		}
+		System.out.println();
+	}
+	
+	public static void startTime() {
+		//time = System.currentTimeMillis();
+		time = System.nanoTime();
+	}
+	public static void printTime() {
+		System.out.println("Approximate runtime: " + (System.nanoTime() - time) + " nanoseconds\n");
 	}
 
 	// Code for Naive Pattern Searching
@@ -129,7 +213,7 @@ public class Main {
 	// Taken from
 	// https://www.geeksforgeeks.org/searching-for-patterns-set-2-kmp-algorithm/
 
-	void KMPSearch(String pat, String txt) {
+	public static void KMPSearch(String pat, String txt) {
 		int M = pat.length();
 		int N = txt.length();
 
@@ -165,7 +249,7 @@ public class Main {
 		}
 	}
 
-	void computeLPSArray(String pat, int M, int lps[]) {
+	public static void computeLPSArray(String pat, int M, int lps[]) {
 		// length of the previous longest prefix suffix
 		int len = 0;
 		int i = 1;
@@ -205,7 +289,7 @@ public class Main {
 	/*
 	 * pat -> pattern txt -> text q -> A prime number
 	 */
-	static void search(String pat, String txt, int q) {
+	public static void searchRabinKarp(String pat, String txt, int q) {
 		int M = pat.length();
 		int N = txt.length();
 		int i, j;
